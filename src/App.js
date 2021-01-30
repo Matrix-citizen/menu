47bbafbb0d3590e5688925b5170e72025ff972dc
Rostyslav <rklyba@yahoo.com>
2021-01-30 11:36:27 +0200

    Configured categories buttons dynamically

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
+
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
 
 function App() {
   const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState(allCategories);
 
   const filterItems = (category) => {
-    if(category === 'all') {
+    if (category === "all") {
       setMenuItems(items);
-      return 
+      return;
     }
-    const newItems = items.filter((item) => item.category === category)
-    setMenuItems(newItems)
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
   };
 
   return (
@@ -23,11 +25,11 @@ function App() {
           <h2>our menu</h2>
           <div className="underline"></div>
         </div>
-        <Categories filterItems={filterItems}/>
-        <Menu items={menuItems}/>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
       </section>
     </main>
-  )
+  );
 }
 
 export default App;
